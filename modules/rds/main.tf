7ae510874e537457b5e8705f4732e1f36b5bcf89
CY110888 <chandramouli.yerraguntla@maxlifeinsurance.com>
2025-09-13 16:14:33 +0530

    chore(fmt): terraform fmt to satisfy CI

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,5 +1,5 @@
 variable "project_prefix" { type = string }
-variable "environment"    { type = string }
+variable "environment" { type = string }
 variable "private_subnet_ids" { type = list(string) }
 variable "rds_sg_id" { type = string }
 variable "db_engine_version" { type = string }
@@ -17,7 +17,7 @@ resource "aws_secretsmanager_secret" "db" {
 }
 
 resource "aws_secretsmanager_secret_version" "db" {
-  secret_id     = aws_secretsmanager_secret.db.id
+  secret_id = aws_secretsmanager_secret.db.id
   secret_string = jsonencode({
     username = "postgres"
     password = random_password.db.result
@@ -30,20 +30,20 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_db_instance" "this" {
-  identifier              = "${var.project_prefix}-${var.environment}-pg"
-  engine                  = "postgres"
-  engine_version          = var.db_engine_version   # <- pass from env tf
-  instance_class          = var.instance_class
-  allocated_storage       = var.allocated_storage
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = [var.rds_sg_id]
-  username                = "postgres"
-  password                = random_password.db.result
-  publicly_accessible     = false
-  skip_final_snapshot     = true
-  multi_az                = false
-  storage_encrypted       = true
+  identifier             = "${var.project_prefix}-${var.environment}-pg"
+  engine                 = "postgres"
+  engine_version         = var.db_engine_version # <- pass from env tf
+  instance_class         = var.instance_class
+  allocated_storage      = var.allocated_storage
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = [var.rds_sg_id]
+  username               = "postgres"
+  password               = random_password.db.result
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  multi_az               = false
+  storage_encrypted      = true
 }
 
 output "db_endpoint" { value = aws_db_instance.this.address }
-output "secret_arn"  { value = aws_secretsmanager_secret.db.arn }
+output "secret_arn" { value = aws_secretsmanager_secret.db.arn }
